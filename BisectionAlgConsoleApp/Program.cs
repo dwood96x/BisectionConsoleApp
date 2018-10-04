24a231bdc71c1d6b2f662aecddf573b4ef3c3ca7
David Wood <dwood96x@gmail.com>
2018-10-04 15:56:13 -0400

    Unfinished

--- BisectionAlgConsoleApp/Program.cs
+++ BisectionAlgConsoleApp/Program.cs
@@ -10,6 +10,38 @@ namespace BisectionAlgConsoleApp
     {
         static void Main(string[] args)
         {
+            BisectLogic bisect = new BisectLogic();
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            bisect.BisectList(list,Console.ReadLine());
+        }
+    }
+    class BisectLogic
+    {
+        public static void BisectList(int[] list,int chosenValue)
+        {
+            bool foundChosenValue = false;
+            do
+            {
+                int searchInt = list.Length / 2;
+                if (list[searchInt] == chosenValue)
+                {
+                    Console.WriteLine($"The chosen value is {chosenValue}");
+                    foundChosenValue = true;
+                }
+                else if (list[searchInt] > chosenValue)
+                {
+                    Console.WriteLine($"The value is lower than {list[searchInt]}");
+                }
+                else
+                {
+                    Console.WriteLine($"The value is greater than {list[searchInt]}");
+                }
+            } while (foundChosenValue == false);
+        }
+        public static int ParsetoInt()
+        {
+            
+            return input
         }
     }
 }
